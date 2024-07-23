3ea050129b0968816db9fb7c49a321fe9d05dbec
Rahmouni Yasser <rahmouniyasser77@gmail.com>
2024-07-23 15:45:32 +0000

    task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" module for task 0 """
+
+
+def makeChange(coins, total):
+    """Returns fewest number of coins needed to meet total"""
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
